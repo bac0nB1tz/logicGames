df873c1a484e45a3014473444d95ea28032ebe50
nic upchurch <nic.upchurch@outlook.com>
2022-10-04 20:36:07 -0400

    lengthOfLongestSubstringWithoutRepeatingCharacters

---
+++ lengthOfLongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace lengthOfLongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/
+            Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
+        }
+
+        static int LengthOfLongestSubstring(string s)
+        {
+            var dummyString = "";
+            var result = -1;
+
+            // Return zero if string is empty
+            if (s.Length == 0)
+                return 0;
+            else if (s.Length == 1)
+                return 1;
+
+            foreach (char c in s)
+            {
+                // If string already contains the character
+                // Then substring after repeating character
+                if (dummyString.Contains(c))
+                {
+                    dummyString = dummyString.Substring(dummyString.IndexOf(c) + 1);
+                }
+                dummyString = dummyString + c;
+                result = Math.Max(dummyString.Length, result);
+            }
+            return result;
+
+        }
+    }
+}
